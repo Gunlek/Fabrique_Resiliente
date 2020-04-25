199ec464e5e0c5d9ef6fc4343ac7d0e8ba3e36ca
Fabien Aubret <fabien.aubret@gmail.com>
2020-04-25 16:01:04 +0200

    [Update] Update compute modules

--- statics/js/vue_compute.js
+++ statics/js/vue_compute.js
@@ -17,7 +17,7 @@ let app = new Vue({
         // Parameters
         nb_mask_per_person: 2,
         mask_market_cost: 3.5,          // This is the market cost of a mask
-        mask_local_cost: 0.5,
+        mask_local_cost: 1,
         startup_delay: 2,
         // Variables
         city_population: null,
@@ -28,12 +28,14 @@ let app = new Vue({
         desired_date_year_str: '',
 
         // Second estimator
+        // Parameters
+        prod_time_centralized: 4*3600,   // In seconds
+        prod_time_semi_centralized: 1.5*3600,   // In seconds
+        production_time_per_machine: 2 * 60, // In seconds
+        // Variables
         sewing_machine_number_estimation: '1',
-        sewing_machine_number: null,
+        sewing_machine_number: 0,
         average_sewing_machine_per_people: 1/1000,
-
-        nb_motiv_centralized_model: 10,
-        nb_motiv_semi_centralized_model_in_workshop: 15
     },
     computed: {
         recommended_mask_number: function(){
@@ -116,13 +118,54 @@ let app = new Vue({
          * Production model algorithms
         */
         computed_sewing_machine_in_city: function(){
+            this.sewing_machine_number = Math.floor(this.city_population * this.average_sewing_machine_per_people);
             return Math.floor(this.city_population * this.average_sewing_machine_per_people);
         },
         local_size_in_square_meter_centralized_model: function(){
-            return 6 * this.nb_motiv_centralized_model;
+            return Math.ceil(6 * 1.3 * this.sewing_machine_number);
         },
         local_size_in_square_meter_semi_centralized_model: function(){
-            return 6 * this.nb_motiv_semi_centralized_model_in_workshop;
-        }
+            return Math.ceil(6 * 0.3 * this.sewing_machine_number);
+        },
+        computed_dayli_production_centralized: function(){
+            return this.sewing_machine_number * this.prod_time_centralized / this.production_time_per_machine;
+        },
+        prod_time_centralized_hours: {
+            get: function(){
+                return this.prod_time_centralized / 3600;
+            },
+            set: function(hours){
+                this.prod_time_centralized = hours * 3600;
+            }
+        },
+        extended_days_centralized_production: function(){
+            return Math.ceil(this.desired_mask_number / (this.computed_dayli_production_centralized)) - this.number_of_days_between_now_and_desired + parseInt(this.startup_delay);
+        },
+        possible_centralized_production_in_time: function(){
+            return this.computed_dayli_production_centralized * (this.number_of_days_between_now_and_desired - parseInt(this.startup_delay))
+        },
+        buy_need_centralized_production: function(){
+            return this.desired_mask_number - this.possible_centralized_production_in_time;
+        },
+        prod_time_semi_centralized_hours: {
+            get: function(){
+                return this.prod_time_semi_centralized / 3600;
+            },
+            set: function(hours){
+                this.prod_time_semi_centralized = hours * 3600;
+            }
+        },
+        computed_dayli_production_semi_centralized: function(){
+            return this.sewing_machine_number * this.prod_time_semi_centralized / this.production_time_per_machine;
+        },
+        extended_days_semi_centralized_production: function(){
+            return Math.ceil(this.desired_mask_number / (this.computed_dayli_production_semi_centralized)) - this.number_of_days_between_now_and_desired + parseInt(this.startup_delay);
+        },
+        possible_semi_centralized_production_in_time: function(){
+            return this.computed_dayli_production_semi_centralized * (this.number_of_days_between_now_and_desired - parseInt(this.startup_delay))
+        },
+        buy_need_semi_centralized_production: function(){
+            return this.desired_mask_number - this.possible_semi_centralized_production_in_time;
+        },
     }
 });
